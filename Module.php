639a6071746b6c928a01e915958e5b987c898475
Rob Allen <rob@akrabat.com>
2017-12-21 15:17:06 +0000

    Separate class for conversion

--- Module.php
+++ Module.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace NFNumberToWord;
 
 use Zend\View\Helper\AbstractHelper;
@@ -7,45 +6,6 @@ use Exception;
 
 class Module extends AbstractHelper
 {
-    protected $dictionary  = array(
-        0                   => 'zero',
-        1                   => 'one',
-        2                   => 'two',
-        3                   => 'three',
-        4                   => 'four',
-        5                   => 'five',
-        6                   => 'six',
-        7                   => 'seven',
-        8                   => 'eight',
-        9                   => 'nine',
-        10                  => 'ten',
-        11                  => 'eleven',
-        12                  => 'twelve',
-        13                  => 'thirteen',
-        14                  => 'fourteen',
-        15                  => 'fifteen',
-        16                  => 'sixteen',
-        17                  => 'seventeen',
-        18                  => 'eighteen',
-        19                  => 'nineteen',
-        20                  => 'twenty',
-        30                  => 'thirty',
-        40                  => 'forty',
-        50                  => 'fifty',
-        60                  => 'sixty',
-        70                  => 'seventy',
-        80                  => 'eighty',
-        90                  => 'ninety',
-        100                 => 'hundred',
-        1000                => 'thousand',
-        1000000             => 'million',
-        1000000000          => 'billion',
-        1000000000000       => 'trillion',
-        1000000000000000    => 'quadrillion',
-        1000000000000000000 => 'quintillion'
-    );
-    
-
     public function getViewHelperConfig()
     {
         return array('services' => array('numberToWord' => $this));
@@ -53,89 +13,7 @@ class Module extends AbstractHelper
 
     public function __invoke($number, $titleCase = false)
     {
-        $dictionary = $this->dictionary;
-        if ($titleCase) {
-            array_walk($dictionary, function (&$item, $key) {
-                $item = ucfirst($item);
-            });
-        }
-
-        return $this->convertNumbersToWords($number, $dictionary);
-    }
-
-    public function convertNumbersToWords($number, $dictionary)
-    {
-        // From: http://www.karlrixon.co.uk/writing/convert-numbers-to-words-with-php/
-
-        $hyphen      = '-';
-        $conjunction = ' and ';
-        $separator   = ', ';
-        $negative    = 'negative ';
-        $decimal     = ' point ';
-        
-        if (!is_numeric($number)) {
-            return false;
-        }
-        
-        if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
-            // overflow
-            throw new Exception(
-                'convertNumbersToWords only accepts numbers between -' . PHP_INT_MAX . ' and ' . PHP_INT_MAX
-            );
-            return false;
-        }
-
-        if ($number < 0) {
-            return $negative . $this->convertNumbersToWords(abs($number), $dictionary);
-        }
-        
-        $string = $fraction = null;
-        
-        if (strpos($number, '.') !== false) {
-            list($number, $fraction) = explode('.', $number);
-        }
-        
-        switch (true) {
-            case $number < 21:
-                $string = $dictionary[$number];
-                break;
-            case $number < 100:
-                $tens   = ((int) ($number / 10)) * 10;
-                $units  = $number % 10;
-                $string = $dictionary[$tens];
-                if ($units) {
-                    $string .= $hyphen . $dictionary[$units];
-                }
-                break;
-            case $number < 1000:
-                $hundreds  = $number / 100;
-                $remainder = $number % 100;
-                $string = $dictionary[$hundreds] . ' ' . $dictionary[100];
-                if ($remainder) {
-                    $string .= $conjunction . $this->convertNumbersToWords($remainder, $dictionary);
-                }
-                break;
-            default:
-                $baseUnit = pow(1000, floor(log($number, 1000)));
-                $numBaseUnits = (int) ($number / $baseUnit);
-                $remainder = $number % $baseUnit;
-                $string = $this->convertNumbersToWords($numBaseUnits, $dictionary) . ' ' . $dictionary[$baseUnit];
-                if ($remainder) {
-                    $string .= $remainder < 100 ? $conjunction : $separator;
-                    $string .= $this->convertNumbersToWords($remainder, $dictionary);
-                }
-                break;
-        }
-        
-        if (null !== $fraction && is_numeric($fraction)) {
-            $string .= $decimal;
-            $words = array();
-            foreach (str_split((string) $fraction) as $number) {
-                $words[] = $dictionary[$number];
-            }
-            $string .= implode(' ', $words);
-        }
-        
-        return $string;
+        $numberToWords = new NumberToWords();
+        return $numberToWords->toWords($number, $titleCase);
     }
 }
